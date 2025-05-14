e0c13c8a8d9d4296c84176a88e9987ec88ea72f5
sam-pc <134122170+sahilmulla@users.noreply.github.com>
2025-05-14 19:18:25 +0530

    uplaod file endpoint

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const authenticateToken = require('./middleware/auth');
 const authHandlers = require('./handlers/auth');
 
+const { File } = require('./models');
+
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/', limits: { fileSize: 1_000 } }); // 1MB
 
@@ -19,7 +21,31 @@ app.post(
 	authenticateToken,
 	upload.single('file'),
 	async (req, res) => {
-		res.send(req.file);
+		try {
+			const user = req.user;
+
+			const uploadedFile = req.file;
+			const { title, description } = req.body ?? {};
+
+			if (!uploadedFile) {
+				return res.status(400).json({ error: 'File is required.' });
+			}
+
+			console.table(uploadedFile);
+
+			const file = await File.create({
+				userId: user.id,
+				originalFilename: uploadedFile.originalname,
+				storagePath: uploadedFile.path,
+				title,
+				description,
+			});
+
+			res.status(200).json({ id: file.id, status: file.status });
+		} catch (error) {
+			console.error(error);
+			res.sendStatus(500);
+		}
 	}
 );
 
